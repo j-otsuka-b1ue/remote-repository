dedb740ac8b2d18d9c0a52ad3dadc5b8fcc55101
JUNYA OTSUKA <j.otsuka.arp@gmail.com>
2023-07-27 15:23:19 +0900

    指摘箇所修正ver_03

--- js_learning/js/json_01.js
+++ js_learning/js/json_01.js
@@ -6,18 +6,14 @@ class Callapi {
   async getData (){
     const data = await fetch('https://dog.ceo/api/breeds/image/random');
     const res = await data.json();
-  }
-  displayDogimage () {
     const dogImage = document.createElement("img");
     document.body.appendChild(dogImage);
     dogImage.id = 'image';
-    dogImage.src = 'https://dog.ceo/api/breeds/image/random';
+    dogImage.src = res.message;
   }
 }
 const clickGetData = document.getElementById('click');
 display.addEventListener('click', () => {
   const getdata = new Callapi();
-  const displaydogimage = new Callapi();
   getdata.getData();
-  displaydogimage.displayDogimage();
 }, false);
