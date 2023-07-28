7130f1f9cc69e4189dea52871b9d1c9166d2bd3d
JUNYA OTSUKA <j.otsuka.arp@gmail.com>
2023-07-28 17:09:21 +0900

    指摘箇所修正ver_05.js

--- js_learning/js/json_01.js
+++ js_learning/js/json_01.js
@@ -5,19 +5,14 @@ class Callapi {
   }
   async getData (){
     const data = await fetch(this.url);
-    //インスタンス化した際の引数部分にURLを渡して、
-    //そのURLを使ってfetchが出来るようにプログラムを組むってことだと思うんですが、
-    //this.url がインスタンス化された際、引数部分のURLが代入され、
-    //それをfetch出来るようにthis.urlとしてみたのですがエラーが出てしまいます。
     const res = await data.json();
     const dogImage = document.createElement("img");
     document.body.appendChild(dogImage);
     dogImage.src = res.message;
   }
 }
-const url1 = new URL('https://dog.ceo/api/breeds/image/random');
 const clickGetData = document.getElementById('click');
 display.addEventListener('click', () => {
-  const getdata = new Callapi();
+  const getdata = new Callapi('https://dog.ceo/api/breeds/image/random');
   getdata.getData();
 }, false);
