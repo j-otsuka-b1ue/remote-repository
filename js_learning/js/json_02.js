b735bda291fb8ce968b5b676f76e64e089d93ece
JUNYA OTSUKA <j.otsuka.arp@gmail.com>
2023-08-01 18:16:05 +0900

    first_commit.js

---
+++ js_learning/js/json_02.js
@@ -0,0 +1,18 @@
+class Callapi {
+  constructor(url,times) {
+    this.url = url;
+    this.times = times;
+  }
+  async getData (){
+    const data = await fetch(this.url + this.times);
+    const res = await data.json();
+    const dogImage = document.createElement("img");
+    document.body.appendChild(dogImage);
+    dogImage.src = res.message;
+  }
+}
+const clickGetData = document.getElementById('click');
+display.addEventListener('click', () => {
+  const getdata = new Callapi('https://dog.ceo/api/breeds/image/random/', '3');
+  getdata.getData();
+}, false);
