ab094054959cb61d98e91e6541ffb6cbb38ddf7d
JUNYA OTSUKA <j.otsuka.arp@gmail.com>
2023-08-10 23:46:12 +0900

    js

--- js_learning/js/general.js
+++ js_learning/js/general.js
@@ -1,28 +1,10 @@
-class PostBlog {
+class Blog {
   constructor () {
     this.submitBtn = document.getElementById('submitBtn');
     this.title = document.getElementById('title');
     this.submitContent = document.getElementById('submitContent');
     this.statusMessage = document.getElementById('statusMessage');
     this.submitBtn.addEventListener('click', this.createPost.bind(this));
-  }
-
-  formValidation () {
-    const titleVal = document.getElementById('errorCreate01');
-    const contentVal = document.getElementById('errorCreate02');
-    errorCreate01.innerHTML = "";
-    errorCreate02.innerHTML = "";
-    if (titleVal.value === "" && contentVal.value === "") {
-      errorCreate01.innerHTML = "タイトルが入力されていません";
-      errorCreate02.innerHTML = "内容が入力されていません";
-    } else if (titleVal.value === "") {
-      errorCreate01.innerHTML = "タイトルが入力されていません";
-    } else if (contentVal.value === "") {
-      errorCreate02.innerHTML = "内容が入力されていません";
-    } else {
-      errorCreate01.innerHTML = "";
-      errorCreate02.innerHTML = "";
-    }
   }
 
   async createPost() {
@@ -34,26 +16,52 @@ class PostBlog {
       body: content,
       userId: 1
     };
-
-    try {
-      const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
-        method: "POST",
-        headers: {
-          'Content-Type':"application/json"
-        },
-        body: JSON.stringify(newPost)
-      });
-
-      if (response.status === 201) {
-        this.statusMessage.innerHTML = "投稿に成功しました(status:201)"
-        this.title.value = "";
-        this.submitContent.value = "";
-      } else {
-        console.error("投稿に失敗しました" + response.status);
+    //バリデーションチェック
+    this.statusMessage.innerHTML = "";
+    const titleVal = document.getElementById('title');
+    const contentVal = document.getElementById('submitContent');
+    errorCreate01.innerHTML = "";
+    errorCreate02.innerHTML = "";
+    if (titleVal.value === "" && contentVal.value === "") {
+      errorCreate01.innerHTML = "↑タイトルが入力されていません";
+      errorCreate02.innerHTML = "↑内容が入力されていません";
+    } else if (titleVal.value === "") {
+      errorCreate01.innerHTML = "↑タイトルが入力されていません";
+    } else if (contentVal.value === "") {
+      errorCreate02.innerHTML = "↑内容が入力されていません";
+    } else {
+      errorCreate01.innerHTML = "";
+      errorCreate02.innerHTML = "";
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+          method: "POST",
+          headers: {
+            'Content-Type':"application/json"
+          },
+          body: JSON.stringify(newPost)
+        });
+  
+        if (response.status === 201) {
+          this.statusMessage.innerHTML = "投稿に成功しました(status:201)"
+          this.title.value = "";
+          this.submitContent.value = "";
+          errorCreate01.innerHTML = "";
+          errorCreate02.innerHTML = "";
+        } else {
+          console.error("投稿に失敗しました" + response.status);
+        }
+      } catch (error) {
+        console.error("エラーが発生しました" + error);
       }
-    } catch (error) {
-      console.error("エラーが発生しました" + error);
     }
   }
+  async getPost() {
+      const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+      const data = response.json();
+      const displayTitle = document.getElementById('title_list');
+      const displayContent = document.getElementById('content_list');
+      displayTitle.innerHTML = data.title;
+      displayContent.innerHTML = data.body;
+  }
 }
-const postBlog = new PostBlog();
+const blog = new Blog();
