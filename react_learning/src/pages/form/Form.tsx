ad46c3e6a97b4f3ac08dddc1b6d518a0def4aaa9
JUNYA OTSUKA <j.otsuka.arp@gmail.com>
2023-09-03 20:16:26 +0900

    form_task_error

--- react_learning/src/pages/form/Form.tsx
+++ react_learning/src/pages/form/Form.tsx
@@ -1,9 +1,70 @@
 import React from "react";
+import { useState } from "react";
 
-export const Form: React.FC = () => {
+const Form = () => {
+  const [val,setVal] = useState("");
+  const handleChange = (e) => {
+    setVal(e.target.value);
+  };
   return (
-    <div className="App">
-      <h1 className="font-bold text-blue-300">ようこそReactフォーム課題へ！</h1>
-    </div>
+    <>
+     <div className = "container">
+      <input type = "text" value = {val} onChange = {handleChange} />
+     </div>
+    </>
+    
+
+
+    // <>
+    // <div className="formContainer">
+    //     <div className="uiForm">
+    //       <div className="formField">
+    //         <label>名前</label>
+    //         <input 
+    //         type="text" 
+    //         placeholder="お名前" 
+    //         name="yourName" 
+    //         value={text} 
+    //         onChange={(event)=>setText(event.target.value)} />
+    //       </div>
+    //       <div className="formField">
+    //         <label>ユーザー名
+    //          <input type="text" placeholder="ユーザーネーム(任意)" name="userName" />
+    //         </label>
+    //       </div>
+    //       <div className="formField">
+    //         <label>メールアドレス</label>
+    //         <input type="text" placeholder="sample@example.com" name="mailAddress" />
+    //       </div>
+    //       <div className="formField">
+    //         <label>パスワード</label>
+    //         <input type="text" placeholder="パスワード" name="password" />
+    //       </div>
+    //       <div className="formField">
+    //         <label>パスワード確認</label>
+    //         <input type="text" placeholder="パスワード確認" name="passwordConf" />
+    //       </div>
+    //       <div className="formField">
+    //         <label>郵便番号</label>
+    //         <input type="text" placeholder="0123456" name="zipCode" />
+    //         <button className="zipSearchBtn">検索</button>
+    //       </div>
+    //       <div className="formField">
+    //         <label>都道府県</label>
+    //         <input type="text" placeholder="都道府県" name="prefectures" />
+    //       </div>
+    //       <div className="formField">
+    //         <label>市区町村</label>
+    //         <input type="text" placeholder="市区町村" name="city" />
+    //       </div>
+    //       <div className="formField">
+    //         <label>番地</label>
+    //         <input type="text" placeholder="番地" name="blockNumber" />
+    //       </div>
+    //     </div>
+    // </div>
+    // </>
   );
-};
+}
+
+export default Form;
